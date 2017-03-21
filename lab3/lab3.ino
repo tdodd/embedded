04a920cbbd92d0a9b2e3860b87df5b1eb196b021
Thomas Dodd <thomas.dodd0@gmail.com>
2017-03-21 15:28:10 -0400

    lab 3 working

--- lab3/lab3.ino
+++ lab3/lab3.ino
@@ -13,7 +13,11 @@
  ******************************************************************************
  */
 
+/**
+ * Libraries
+ */
 #include <SoftwareSerial.h>
+#include <Wire.h>
 
 /**
  * Global pin values.
@@ -23,6 +27,9 @@ const int LCD_PIN = 18;
 const int RIGHT_MOTOR_PIN = 45;
 const int LEFT_MOTOR_PIN = 8;
 const int SONAR_PIN = 22;
+const int reg = 0x01;
+
+#define TEMPSENSOR 0x68 //Address
 
 /**
  * Forward and backward movement
@@ -42,30 +49,35 @@ const char PENG_NUM[] = "4229132";
  * Timing constants.
  */
 const int FULL_TURN_MILLI = 3500;
-const int TILE_DISTANCE = 1200;
+const int TILE_DISTANCE = 1500;
 
 /**
- * printToFirst, Prints text on the 1st line of the LCD display.
+ * Instances
+ */
+SoftwareSerial LCD(0, LCD_PIN);
+
+/**
+ * printToFirst, Prints centered text on the 1st line of the LCD display.
  * 
  * @param *LCD, a pointer to the LCD screen.
  * @param str, the string to display.
  */
-void printToFirst(SoftwareSerial* LCD, char str[]) {
-  LCD->write(0xFE);
-  LCD->write(128 + getOffset(strlen(str)));
-  LCD->print(str);
+void printToFirst(char str[]) {
+  LCD.write(0xFE);
+  LCD.write(128 + getOffset(strlen(str)));
+  LCD.print(str);
 }
 
 /**
- * printToSecond, Prints text on the 2nd line of the LCD display.
+ * printToSecond, Prints centered text on the 2nd line of the LCD display.
  * 
  * @param *LCD, a pointer to the LCD screen.
  * @param str, the string to display.
  */
-void printToSecond(SoftwareSerial* LCD, char str[]) {
-  LCD->write(0xFE);
-  LCD->write(64 + 128 + getOffset(strlen(str)));
-  LCD->print(str);
+void printToSecond(char str[]) {
+  LCD.write(0xFE);
+  LCD.write(64 + 128 + getOffset(strlen(str)));
+  LCD.print(str);
 }
 
 /**
@@ -80,18 +92,15 @@ int getOffset(int str_size) {
 
 /**
  * clearLcd, Clears the LCD screen.
- * 
- * @param *LCD, a pointer to the LCD screen.
  */
-void clearLcd(SoftwareSerial* LCD) {
-  LCD->write(0xFE);
-  LCD->write(0x01);
+void clearLcd() {
+  LCD.write(0xFE);
+  LCD.write(0x01);
   delay(10);  
 }
 
 /**
  * blinkLed, Blinks the led a specified number of times.
- * 
  * @param num, the number of times to blink the led.
  */
 void blinkLed(int num) {
@@ -105,18 +114,31 @@ void blinkLed(int num) {
 
 /**
  * moveForward, Moves the robot forward for a specified amount of time and prints action to LCD.
- * 
- * @param micro_sec, the time in microseconds for the wheels to turn.
+ * @param milli_sec, the time in milliseconds for the wheels to turn.
  * @param *LCD, a pointer to the LCD screen.
  */
-void moveForward(int milli_sec, SoftwareSerial* LCD) {
-  clearLcd(LCD);
-  printToFirst(LCD, "Moving");
-  printToSecond(LCD, "Forward");
+void moveForward(int milli_sec) {
+  clearLcd();
+  printToFirst("Moving");
+  printToSecond("Forward");
   analogWrite(RIGHT_MOTOR_PIN, RIGHT_MOTOR_FORWARD_PULSE);
   analogWrite(LEFT_MOTOR_PIN, LEFT_MOTOR_FORWARD_PULSE);
   delay(milli_sec);
-  stop(LCD);
+  stop();
+}
+
+/**
+ * reverse, Reverses the robot for a specified of time and prints action to LCD.
+ * @param milli_sec, the time in milliseconds for the wheels to turn.
+ */
+void reverse(int milli_sec) {
+  clearLcd();
+  printToFirst("Moving");
+  printToSecond("Backwards");
+  analogWrite(RIGHT_MOTOR_PIN, RIGHT_MOTOR_BACKWARD_PULSE);
+  analogWrite(LEFT_MOTOR_PIN, LEFT_MOTOR_BACKWARD_PULSE);
+  delay(milli_sec);
+  stop();
 }
 
 /**
@@ -125,14 +147,14 @@ void moveForward(int milli_sec, SoftwareSerial* LCD) {
  * @param increment, the # of increments of 45 degrees to turn.
  * @param *LCD, a pointer to the LCD screen.
  */
-void turnRight(int increment, SoftwareSerial* LCD) {
-  clearLcd(LCD);
-  printToFirst(LCD, "Rotating");
-  printToSecond(LCD, "Right");
+void turnRight(int increment) {
+  clearLcd();
+  printToFirst("Rotating");
+  printToSecond("Right");
   analogWrite(RIGHT_MOTOR_PIN, RIGHT_MOTOR_FORWARD_PULSE);
   analogWrite(LEFT_MOTOR_PIN, LEFT_MOTOR_BACKWARD_PULSE);
   delay(FULL_TURN_MILLI * increment/8);
-  stop(LCD);
+  stop();
 }
 
 /**
@@ -141,26 +163,37 @@ void turnRight(int increment, SoftwareSerial* LCD) {
  * @param increment, the # of increments of 45 degrees to turn.
  * @param *LCD, a pointer to the LCD screen.
  */
-void turnLeft(int increment, SoftwareSerial* LCD) {
-  clearLcd(LCD);
-  printToFirst(LCD, "Rotating");
-  printToSecond(LCD, "left");
+void turnLeft(int increment) {
+  clearLcd();
+  printToFirst("Rotating");
+  printToSecond("left");
   analogWrite(RIGHT_MOTOR_PIN, RIGHT_MOTOR_BACKWARD_PULSE);
   analogWrite(LEFT_MOTOR_PIN, LEFT_MOTOR_FORWARD_PULSE);
   delay((FULL_TURN_MILLI - 350) * increment/8);
-  stop(LCD);
+  stop();
 }
 
 /**
  * stop, Stops the current motion and prints action to LCD.
- *
  * @param *LCD, a pointer to the LCD screen.
  */
-void stop(SoftwareSerial* LCD) {
-  clearLcd(LCD);
-  printToFirst(LCD, "Stopping");
+void stop() {
+  clearLcd();
+  printToFirst("Stopping");
   analogWrite(RIGHT_MOTOR_PIN, 0);
   analogWrite(LEFT_MOTOR_PIN, 0);
+}
+
+/**
+ * Pre-programmed collision avoidance algorithm.
+ */
+void avoidCollision() {
+  printTemperature();
+  delay(3000);
+  reverse(TILE_DISTANCE);
+  turnRight(2);
+  moveForward(TILE_DISTANCE * 2);
+  turnLeft(2);
 }
 
 /**
@@ -174,30 +207,57 @@ int ping() {
   delayMicroseconds(2);
   digitalWrite(SONAR_PIN, HIGH);
   delayMicroseconds(5);
-  digitalWrite(SONAR_PIN, LOW)
+  digitalWrite(SONAR_PIN, LOW);
   pinMode(SONAR_PIN, INPUT);
 
   pinMode(SONAR_PIN, INPUT);
-  duration = pulseIn(SONAR_PIN, HIGH);
+  int duration = pulseIn(SONAR_PIN, HIGH);
   
-  inches = duration / 74 / 2;
-  cm = duration / 29 / 2;
+  int inches = duration / 74 / 2;
+  int cm = duration / 29 / 2;
+
+  return cm;
+}
+
+/**
+ * Print the current ambient temperature.
+ */
+int printTemperature() {
+  Wire.beginTransmission(TEMPSENSOR);
+  Wire.write(reg); // Indicate temperature value to read
+  Wire.endTransmission();
+  Wire.requestFrom(TEMPSENSOR, 1); // Request data
+  while(Wire.available() < 1); // Wait for data
+  byte temperatureData = Wire.read(); // Temp. value
+  delay(50);
+
+  // Print to screen.
+  printToFirst("Temperature");
+  LCD.write(0xFE);
+  LCD.write(64 + 128 + 7);
+  LCD.print(temperatureData);
 }
 
 void setup() {
   // Set pins for data transmission (output).
   pinMode(LED_PIN, OUTPUT);
   pinMode(RIGHT_MOTOR_PIN, OUTPUT);
-  pinMode(LEFT_MOTOR_PIN, OUTPlUT);
+  pinMode(LEFT_MOTOR_PIN, OUTPUT);
+  
+  LCD.begin(9600); // open serial port to write at 9600 bps.
+  Wire.begin(); // Join I2C bus.
 }
 
 void loop() {
-  SoftwareSerial LCD = SoftwareSerial(0, LCD_PIN);
-  LCD.begin(9600); // open serial port to write at 9600 bps.
-  
-  // Print student numbers for 5 seconds.
-  printToFirst(&LCD, THOMAS_NUM);
-  printToSecond(&LCD, PENG_NUM);
-  blinkLed(5);
-  clearLcd(&LCD);
+
+  // Move forward 1 tile.
+  moveForward(TILE_DISTANCE);
+  delay(1000);
+
+  // Check distance.
+  if (ping() < 20) {
+    avoidCollision();
+  }
+
+
 }
